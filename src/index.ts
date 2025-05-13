30fe647f132ca25353e5ae0d06b36efbd3e60199
Senadhian <senadhian@gmail.com>
2025-05-13 09:25:13 +0530

    Modified filter messages strategy

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { Request, response, Response } from "express";
 import { langGraph } from "./lang-graph/app.js";
-import { HumanMessage } from "@langchain/core/messages";
+import { BaseMessage, HumanMessage, filterMessages} from "@langchain/core/messages";
 import { PostgresSaver } from "@langchain/langgraph-checkpoint-postgres";
 import { pool } from "./pg-sql/pg-connection.js";
+import {v4 as uuidv4} from "uuid";
 
 const app = express();
 
@@ -22,7 +23,10 @@ app.post("/chat", async (req: Request, res: Response) => {
   console.log("Received message:", query);
   console.log("Received threadId:", threadId);
   const config = { configurable: { thread_id: threadId } };
-  const message = new HumanMessage(query);
+  const message = new HumanMessage({
+    id: uuidv4(),
+    content: query,
+  });
  
   const response = await langGraph.invoke({ messages: [message] }, config);
   console.log("Response:", response.messages[response.messages.length - 1].content);
@@ -36,11 +40,19 @@ app.post("/chat/:threadId", async(req: Request, res: Response) => {
   const checkpointer = new PostgresSaver(pool);
   const config = { configurable: { thread_id: threadId } };
   // const messages = checkpointer.list(config)
-
-  const message =  await checkpointer.get(config);
   console.log(`Received thread ${threadId}`);
+  // const messages =  await checkpointer.get(config);
+  let messages =  (await langGraph.getState(config)).values.messages;
 
-  console.log("Messages:", message);
+  const filteredMesagesResponse = filterMessages(messages, {
+    includeTypes : ["human", "ai"],
+  })
+
+  const filteredMesagesWithAiResponseResponse = filteredMesagesResponse.filter((message: BaseMessage) => !!message.content );
+
+  console.log("Messages:", messages);
+
+  console.log("Filtered Messages:", filteredMesagesResponse);
   
   // for await (const message of messages) {
   //   console.log(message);
@@ -48,7 +60,7 @@ app.post("/chat/:threadId", async(req: Request, res: Response) => {
 
 
   res.send({
-    response: message,
+    response: filteredMesagesWithAiResponseResponse,
   })
 });
 
